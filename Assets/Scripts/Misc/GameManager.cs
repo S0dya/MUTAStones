d79712a773d31b1df0bd3605194c6dc81540d7cc
Cowpoke2 <pepsipsycho21@gmail.com>
2024-01-27 01:19:30 +0400

    misc scripts and finishing setup

---
+++ Assets/Scripts/Misc/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using UnityEngine.UI;
+using TMPro;
+
+[DefaultExecutionOrder(-1)]
+public class GameManager : SingletonMonobehaviour<GameManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+    }
+}
---
+++ Assets/Scripts/Misc/LoadingScene.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Linq;
+
+public class LoadingScene : SingletonMonobehaviour<LoadingScene>
+{
+    [SerializeField] GameObject LoadingScreen;
+    [SerializeField] Image LoadingBarFill;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //StartCoroutine(LoadMenu());
+    }
+
+    //outside methods
+    public void OpenMenu(int sceneToClose)
+    {
+        SceneManager.UnloadSceneAsync(sceneToClose);
+        StartCoroutine(LoadMenu());
+    }
+    public void OpenMenu() => OpenMenu(Settings.curSceneId);
+
+    public void OpenScene(int sceneToOpen)
+    {
+        StartCoroutine(LoadGame(sceneToOpen));
+    }
+
+    //main cors
+    IEnumerator LoadMenu()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+
+        ToggleLoadingScreen(true);
+
+        while (!operation.isDone)
+        {
+            SetFillAmount(operation.progress);
+
+            yield return null;
+        }
+
+        ToggleLoadingScreen(false);
+    }
+
+    IEnumerator LoadGame(int sceneToOpen)
+    {
+        SceneManager.UnloadSceneAsync(1);
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneToOpen, LoadSceneMode.Additive);
+
+        ToggleLoadingScreen(true);
+
+        while (!operation.isDone)
+        {
+            SetFillAmount(operation.progress);
+
+            yield return null;
+        }
+
+        ToggleLoadingScreen(false);
+    }
+
+    //other methods
+    void ToggleLoadingScreen(bool toggle) => LoadingScreen.SetActive(toggle);
+    void SetFillAmount(float progress) => LoadingBarFill.fillAmount = Mathf.Clamp01(progress / 0.9f);
+}
